0f721c1d071e181296eac5578e8a8eb49cec946b
Roberto Medina <robby.medina562@gmail.com>
2019-08-24 08:20:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
+def sort_array_asc(integers)
+  integers.sort
+end
 
+def sort_array_desc(integers)
+  integers.sort.reverse
+end
+  
+  def sort_array_char_count(strings)
+    strings.sort {|letters| letters.length, String.CASE_INSENSITIVE_ORDER}
+
+end
